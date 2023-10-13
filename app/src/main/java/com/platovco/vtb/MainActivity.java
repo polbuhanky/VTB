d20cccd0cc500cc70ca05d0db387d782ad06f2db
polbuhanky <78269098+polbuhanky@users.noreply.github.com>
2023-10-13 19:10:43 +0300

    1

--- app/src/main/java/com/platovco/vtb/MainActivity.java
+++ app/src/main/java/com/platovco/vtb/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
         MapFragment mapFragment = new MapFragment();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.big_container, mapFragment);
