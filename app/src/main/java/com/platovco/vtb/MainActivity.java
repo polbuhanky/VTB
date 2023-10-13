01896acd1a912a7686e091161210014daa0d1991
polbuhanky <78269098+polbuhanky@users.noreply.github.com>
2023-10-13 19:08:38 +0300

    1

---
+++ app/src/main/java/com/platovco/vtb/MainActivity.java
@@ -0,0 +1,20 @@
+package com.platovco.vtb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MapFragment mapFragment = new MapFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.big_container, mapFragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/platovco/vtb/MapViewModel.java
@@ -0,0 +1,7 @@
+package com.platovco.vtb;
+
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
